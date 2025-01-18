c6685778386eb43c52a65f61ae65762a6566611a
DigvijayDD <digvijayd2020@gmail.com>
2025-01-18 19:14:28 +0530

    Project Competed

---
+++ Backend/src/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import {connectDB} from "./lib/db.js";
+import path from "path";
+
+
+import authRoutes from "./route/auth.route.js";
+import messageRoutes from "./route/message.route.js";
+import {app,server} from "./lib/socket.js";
+
+dotenv.config()
+
+app.use(cookieParser());
+
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
+
+const PORT = process.env.PORT ;
+const _dirname = path.resolve();
+
+console.log("Express JSON Middleware Initialized");
+app.use((req, res, next) => {
+    console.log("Request Body before express.json:", req.body);
+    next();
+});
+
+// app.use(express.json());
+app.use(express.json({ limit: "50mb" })); // JSON payload limit
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname, "../Frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(_dirname, "../Frontend/dist/index.html"));
+    });
+}
+server.listen(PORT,()=>{
+console.log("Server is running on port:" + PORT);
+connectDB(); 
+});
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,49 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",         // Include the root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/TS/JSX/TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+};
